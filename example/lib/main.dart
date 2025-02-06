42d3c8410f2891a25dad7665df7a0beb0333d63d
Abhijith C <abhijith@eduport.app>
2025-02-06 15:16:04 +0530

    quality fetch issue fixes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,7 +31,7 @@ class _SamplePlayerState extends State<SamplePlayer> {
       startAt: Duration(seconds: 400),
       videoPlayerController: VideoPlayerController.networkUrl(
         Uri.parse(
-          "https://d357lqen3ahf81.cloudfront.net/transcoded/3xs3Q4czfzH/video.m3u8",
+          "https://d357lqen3ahf81.cloudfront.net/transcoded/6FNnH2Mcznp/video.m3u8",
         ),
         //https://d357lqen3ahf81.cloudfront.net/transcoded/BYQBKK3tKhH/video.m3u8 (240p_h264/video.m3u8)
         formatHint: VideoFormat.hls,
